ec0660caa1d7a1acd080de1be4df5c06a1fe7378
devHikary <wrkkarina@gmail.com>
2021-07-11 20:43:59 -0300

    Aula 2.6

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
+import { ModalConfig } from './interfaces/modal-config';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent{
+
+  public config: ModalConfig;
 
   constructor() { }
-
-  ngOnInit(): void {
-  }
 
 }
