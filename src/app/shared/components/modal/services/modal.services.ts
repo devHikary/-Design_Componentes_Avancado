c399d0081c66735afc65234377049eb6583ade12
devHikary <wrkkarina@gmail.com>
2021-07-13 21:50:11 -0300

    Aula 2.10

--- src/app/shared/components/modal/services/modal.services.ts
+++ src/app/shared/components/modal/services/modal.services.ts
@@ -1,17 +1,37 @@
-import { Injectable, TemplateRef } from '@angular/core';
+import { ComponentFactory, ComponentFactoryResolver, ComponentRef, Injectable, Injector, TemplateRef } from '@angular/core';
 import { ModalConfig } from '../interfaces/modal-config';
+import { ModalComponent } from '../modal.component';
 
 @Injectable()
 export class ModalService{
 
+  private componentFactory: ComponentFactory<ModalComponent>;
+
+  constructor (
+    componentFactoryResolver: ComponentFactoryResolver,
+    private injector: Injector,
+    ){
+    componentFactoryResolver.resolveComponentFactory(ModalComponent);
+  }
+
   public open(config: ModalConfig): ModalRef{
-    console.log("open called");
-    return new ModalRef();
+    const componentRef = this.createComponentRef();
+    componentRef.instance.config  = config;
+    return new ModalRef(componentRef);
+  }
+
+  private createComponentRef(): ComponentRef<ModalComponent>{
+    return this.componentFactory.create(this.injector);
   }
 }
 
 export class ModalRef{
+
+  constructor(private componentRef: ComponentRef<ModalComponent>){
+
+  }
   public close(): void{
     console.log("close called");
+    this.componentRef.destroy();
   }
 }
