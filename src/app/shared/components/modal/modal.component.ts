4dcb7b4cf980591c93efbf75775c3e6b9c6d4fbf
devHikary <wrkkarina@gmail.com>
2021-07-15 21:33:30 -0300

    Aula 3.6

--- src/app/shared/components/modal/modal.component.ts
+++ src/app/shared/components/modal/modal.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit, TemplateRef } from '@angular/core';
+import { Component, HostBinding, OnInit, TemplateRef } from '@angular/core';
+import { fade } from '../../animations/fade';
 import { ModalConfig } from './interfaces/modal-config';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
+  animations: [fade]
 })
 export class ModalComponent{
 
+  @HostBinding('@fade') fade = true;
   public config: ModalConfig;
 
   constructor() { }
