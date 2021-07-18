d714dfd89507268a26a052cb54c2765ec0604c41
devHikary <wrkkarina@gmail.com>
2021-07-18 16:45:55 -0300

    Aula 4.5

--- src/app/shared/components/modal/modal.module.ts
+++ src/app/shared/components/modal/modal.module.ts
@@ -1,11 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+
+import { FocusTrapModule } from '../../directives/focus-trap/focus-trap.module';
 import { ModalComponent } from './modal.component';
 import { ModalService } from './services/modal.services';
 
 @NgModule({
   declarations: [ModalComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    FocusTrapModule
+  ],
   exports: [ModalComponent],
   providers: [ModalService]
 })
