9cfdc54decf1749fa850f10e7185bd41eb437505
devHikary <wrkkarina@gmail.com>
2021-07-18 21:06:33 -0300

    Aula 5.6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,27 +1,49 @@
-import { Component, TemplateRef, ViewChild } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ModalRef } from './shared/components/modal/models/modal-ref';
-import {ModalService } from './shared/components/modal/services/modal.services';
+import { ModalService } from './shared/components/modal/services/modal.services';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
   @ViewChild('modal') public modalTemplateRef: TemplateRef<any>;
   title = 'a11y-p2';
   public firstName = 'Karina';
   public modalRef: ModalRef;
   public info = false;
+  public form: FormGroup;
 
 
-  constructor(private modalService: ModalService){}
+  constructor(
+    private modalService: ModalService,
+    private formBuilder: FormBuilder,
+  ) { }
 
-  public show(): void{
+  public ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      firstName: ['Flávio', Validators.required],
+      surname: ['', Validators.required],
+      age: ['', Validators.required],
+      info: [false]
+    });
+  }
+
+  public show(): void {
     this.modalRef = this.modalService.open({
       templateRef: this.modalTemplateRef,
       title: 'User Details',
     });
+  }
+
+  public submit(): void {
+    if(this.form.invalid) {
+      return;
+    }
+    console.log(this.form.value);
+    this.modalRef.close();
   }
 }
